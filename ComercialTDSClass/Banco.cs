aa84321c5f5706b6051388bf2087937056d25050
ErikCarvalho1 <erikcarvalhosilva2@gmail.com>
2025-05-08 20:41:25 -0300

    Adicionar conexão MySQL e nova label no formulário
    Implementado método `Abrir` em `Banco.cs` para gerenciar a conexão com o banco de dados MySQL `comercialdb`, incluindo tratamento de exceções. Adicionada uma nova `Label` chamada `label1` em `FrmPrincipal.Designer.cs`, configurada com propriedades visuais e integrada ao layout do formulário, que agora é um contêiner MDI.

--- ComercialTDSClass/Banco.cs
+++ ComercialTDSClass/Banco.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,23 @@ namespace ComercialTDSClass
 {
     class Banco
     {
+        public static MySqlCommand Abrir()
+        {
+
+
+            MySqlCommand cmd = new();
+            string strConn = @"server=Localhost;database=comercialdb;user=root;password=";
+            MySqlConnection cn = new(strConn);
+            try
+            {
+                cn.Open();
+                cmd.Connection = cn;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return cmd;
+        }
     }
 }
--- ComercialTDSDesk/FrmPrincipal.Designer.cs
+++ ComercialTDSDesk/FrmPrincipal.Designer.cs
@@ -28,21 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 72F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(172, 127);
+            label1.Name = "label1";
+            label1.Size = new Size(442, 119);
+            label1.TabIndex = 1;
+            label1.Text = "sistema";
             // 
             // FrmPrincipal
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
             IsMdiContainer = true;
             Name = "FrmPrincipal";
             Text = "Sistema Comercial TDS T01";
             WindowState = FormWindowState.Maximized;
             Load += FrmPrincipal_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
     }
 }
