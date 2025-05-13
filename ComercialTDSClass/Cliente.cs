bf3dad8be0afc008dc107a6b0865a2a744889510
ERIK CARVALHO DA SILVA <erik.csilva7@senacsp.edu.br>
2025-05-13 09:14:52 -0300

    termino dos metodos das clases Cliente e Endereco

--- ComercialTDSClass/Cliente.cs
+++ ComercialTDSClass/Cliente.cs
@@ -8,118 +8,122 @@ using System.Threading.Tasks;
 
 namespace ComercialTDSClass
 {
-    class Cliente
+    public class Cliente
     {
-
-
         public int Id { get; set; }
-
-        public string Nome { get; set; }
-
-        public string Cpf { get; set; }
-
-        public string Telefone { get; set; }
-
-        public string Email { get; set; }
-
-        public DateTime DataNasc { get; set; }
-
-        public DateTime DataCad { get; set; }
-
+        public string? Nome { get; set; }
+        public string? Cpf { get; set; }
+        public string? Telefone { get; set; }
+        public string? Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public DateTime DataCadastro { get; set; }
         public bool Ativo { get; set; }
-
+        public List<Endereco>? Enderecos { get; set; }
         public Cliente() { }
-
-        //listar
-        public Cliente(int id, string nome, string cpf, string telefone, string email, DateTime dataNasc, DateTime dataCad, bool ativo)
+        public Cliente(string nome, string cpf, string telefone, string email, DateTime dataNascimento, DateTime dataCadastro, bool ativo, List<Endereco>? enderecos)
+        {// este é o construtor que utilizaremos para inserir o cliente
+            Nome = nome;
+            Cpf = cpf;
+            Telefone = telefone;
+            Email = email;
+            DataNascimento = dataNascimento;
+        }
+        public Cliente(int id, string nome, string cpf, string telefone, string email, DateTime dataNascimento, DateTime dataCadastro, bool ativo, List<Endereco>? enderecos)
         {
             Id = id;
             Nome = nome;
             Cpf = cpf;
             Telefone = telefone;
             Email = email;
-            DataNasc = dataNasc;
-            DataCad = dataCad;
+            DataNascimento = dataNascimento;
+            DataCadastro = dataCadastro;
             Ativo = ativo;
+            Enderecos = enderecos;
         }
-
-
-        //inserir 
-        public Cliente(string nome, string cpf, string telefone, string email, DateTime dataNasc, DateTime dataCad, bool ativo)
+        public Cliente(int id, string nome, string cpf, string telefone, string email, DateTime dataNascimento, DateTime dataCadastro, bool ativo)
         {
-
+            Id = id;
             Nome = nome;
             Cpf = cpf;
             Telefone = telefone;
             Email = email;
+            DataNascimento = dataNascimento;
+            DataCadastro = dataCadastro;
             Ativo = ativo;
         }
-    
-        public Cliente(string nome, string telefone, DateTime dataNasc, DateTime dataCad, bool ativo)
-        {
-
-            Nome = nome;
-            Telefone = telefone;
-            DataNasc = dataNasc;
-            DataCad = dataCad;
-            Ativo = ativo;
-        }
-
         public void Inserir()
         {
             var cmd = Banco.Abrir();
             cmd.CommandType = CommandType.StoredProcedure;
-            cmd.CommandText = "sp_usuario_insert";
+            cmd.CommandText = "sp_cliente_insert";
             cmd.Parameters.AddWithValue("spnome", Nome);
             cmd.Parameters.AddWithValue("spcpf", Cpf);
             cmd.Parameters.AddWithValue("sptelefone", Telefone);
             cmd.Parameters.AddWithValue("spemail", Email);
-
-            Id = Convert.ToInt32(cmd.ExecuteScalarAsync());
+            cmd.Parameters.AddWithValue("spdatanasc", DataNascimento);
+            Id = Convert.ToInt32(cmd.ExecuteScalar());
             cmd.Connection.Close();
         }
         public bool Atualizar()
         {
-
             var cmd = Banco.Abrir();
             cmd.CommandType = CommandType.StoredProcedure;
-            cmd.CommandText = "sp_usuario_altera";
+            cmd.CommandText = "sp_cliente_update";
             cmd.Parameters.AddWithValue("spid", Id);
-            cmd.Parameters.AddWithValue("spnopme", Nome);
-            cmd.Parameters.AddWithValue("spcpf", Cpf);
+            cmd.Parameters.AddWithValue("spnome", Nome);
             cmd.Parameters.AddWithValue("sptelefone", Telefone);
-            cmd.Parameters.AddWithValue("spemail", Email);
-
-            //usuando if ternario, sem fechar conexao 
+            cmd.Parameters.AddWithValue("spdatanasc", DataNascimento);
             return cmd.ExecuteNonQuery() > 0 ? true : false;
-
-
         }
         public static Cliente ObterPorId(int id)
         {
             Cliente cliente = new();
             var cmd = Banco.Abrir();
-            cmd.CommandText = $"select * from clientes where id = '{id}'";
+            cmd.CommandText = $"select * from clientes where id = {id}";
             var dr = cmd.ExecuteReader();
             if (dr.Read())
             {
                 cliente = new(
-                                dr.GetInt32(0),
-                                dr.GetString(1),
-                                dr.GetString(2),
-                                dr.GetString(3),
-                                dr.GetString(4),
-                                dr.GetDateTime(5),
-                                dr.GetDateTime(6),
-                                dr.GetBoolean(7)
-                );
+                            dr.GetInt32(0),
+                            dr.GetString(1),
+                            dr.GetString(2),
+                            dr.GetString(3),
+                            dr.GetString(4),
+                            dr.GetDateTime(4),
+                            dr.GetDateTime(5),
+                            dr.GetBoolean(6),
+                            Endereco.ObterListaPorClienteId(dr.GetInt32(0))
+                        );
             }
             dr.Close();
+            cmd.Connection.Close();
             return cliente;
         }
-        public List<Endereco> ObterEnderecos()
+        public static List<Cliente> ObterLista()
         {
-            return Endereco.ObterListaPorClienteId(this.Id);
+            List<Cliente> clientes = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandText = $"select * from clientes order by nome";
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                clientes.Add(new(
+                              dr.GetInt32(0),
+                            dr.GetString(1),
+                            dr.GetString(2),
+                            dr.GetString(3),
+                            dr.GetString(4),
+                            dr.GetDateTime(4),
+                            dr.GetDateTime(5),
+                            dr.GetBoolean(6),
+                            Endereco.ObterListaPorClienteId(dr.GetInt32(0))
+                        )
+                    );
+            }
+            dr.Close();
+            cmd.Connection.Close();
+            return clientes;
         }
+
     }
 }
